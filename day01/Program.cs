47ae76258b72ef7b282b1f337b3549d7d8e9b704
Jyborn <alexander.jyborn@gmail.com>
2024-12-02 11:47:34 +0100

    day1, docker not working

---
+++ day01/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using day01;
+
+var part = Environment.GetEnvironmentVariable("part");
+
+var col1 = new List<int>();
+var col2 = new List<int>();
+using FileStream fs = new ("input.txt", FileMode.Open, FileAccess.Read);
+using StreamReader sr = new(fs);
+string? line;
+while ((line = sr.ReadLine()) != null)
+{
+    var cols = line.Split(new []{' ', '\t'}, StringSplitOptions.RemoveEmptyEntries);
+    col1.Add(int.Parse(cols[0]));
+    col2.Add(int.Parse(cols[1]));
+}
+
+var solution = part switch
+{
+    "part1" => Puzzle.Part1(col1, col2),
+    "part2" => Puzzle.Part2(col1, col2),
+    _ => throw new ArgumentOutOfRangeException($"unexpected part {part}")
+};
+
+Console.WriteLine($"Solution: {solution}");
+
+
+
---
+++ day01/Puzzle.cs
@@ -0,0 +1,18 @@
+﻿namespace day01;
+
+public class Puzzle
+{
+    public static int Part1(List<int> col1, List<int> col2)
+    {
+        col1.Sort();
+        col2.Sort();
+        var totalDiff = col1.Zip(col2, (x, y) => Math.Abs(x - y)).Sum();
+        return totalDiff;
+    }
+
+    public static int Part2(List<int> col1, List<int> col2)
+    {
+        var col2Uniques = col2.GroupBy(x => x).Select(g => (Number: g.Key, Count: g.Count())).ToDictionary();
+        return col1.Select(x => col2Uniques.TryGetValue(x, out var occurrences) ? occurrences * x : 0 ).Sum();
+    }
+}
