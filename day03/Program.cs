f04f942ac25cea8560474f7a48daaac2d507acf2
Jyborn <alexander.jyborn@gmail.com>
2024-12-03 08:51:56 +0100

    day3

---
+++ day03/Program.cs
@@ -0,0 +1,23 @@
+﻿using day03;
+
+string content;
+try
+{
+    using FileStream fs = new ("input.txt", FileMode.Open, FileAccess.Read);
+    using StreamReader sr = new (fs);
+    content = sr.ReadToEnd();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+var part = Environment.GetEnvironmentVariable("part");
+var solution = part switch
+{
+    "part1" => Puzzle.Part1(content),
+    "part2" => Puzzle.Part2(content),
+    _ => throw new ArgumentOutOfRangeException($"unexpected part {part}")
+};
+
+Console.WriteLine($"Solution: {solution}");
---
+++ day03/Puzzle.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace day03;
+
+public class Puzzle
+{
+    public static int Part1(string content)
+    {
+        var multiplications = ParseInput(content);
+        return multiplications.Select((x) => x.Item1 * x.Item2).Sum();
+    }
+
+    private static List<(int, int)> ParseInput(string content)
+    {
+        const string pattern = @"mul\((\d+),(\d+)\)";
+        
+        var results = new List<(int x, int y)>();
+        
+        foreach (Match match in Regex.Matches(content, pattern))
+        {
+            var x = int.Parse(match.Groups[1].Value);
+            var y = int.Parse(match.Groups[2].Value);
+            
+            results.Add((x, y));
+        }
+
+        return results;
+    }
+    public static int Part2(string content)
+    {
+        var multiplications = ParseInput(RemoveDontInstructions(content));
+        return multiplications.Select((x) => x.Item1 * x.Item2).Sum();;
+    }
+
+    private static string RemoveDontInstructions(string input)
+    {
+        int start, end;
+        const string startMark = "don't()";
+        const string endMark = "do()";
+        while ((start = input.IndexOf(startMark, StringComparison.Ordinal)) != -1)
+        {
+            end = input.IndexOf(endMark, start, StringComparison.Ordinal);
+            if (end != -1)
+            {
+                input = input.Remove(start, end + endMark.Length - start);   
+            }
+            else
+            {
+                input = input.Remove(start);
+                break;
+            }
+        }
+        Console.WriteLine(input);
+        return input;
+    }
+}
