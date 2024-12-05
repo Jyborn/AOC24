18bbcb74ed74b38e73f53ad6299c5a47cd16060c
Jyborn <alexander.jyborn@gmail.com>
2024-12-05 10:51:42 +0100

    day5

---
+++ day05/Program.cs
@@ -0,0 +1,45 @@
+﻿using day05;
+
+var rules = new List<(int, int)>();
+var data = new List<List<int>>();
+
+using FileStream fs = new ("input.txt", FileMode.Open, FileAccess.Read);
+using StreamReader sr = new (fs);
+bool isParsingRules = true;
+
+string? line;
+while ((line = sr.ReadLine()) != null)
+{
+    var trimmedLine = line.Trim();
+
+    if (string.IsNullOrEmpty(trimmedLine))
+    {
+        isParsingRules = false;
+        continue;
+    }
+
+    if (isParsingRules)
+    {
+        var parts = trimmedLine.Split('|');
+        rules.Add((int.Parse(parts[0]), int.Parse(parts[1])));
+    }
+    else
+    {
+        var numbers = trimmedLine.Split(',')
+            .Select(int.Parse)
+            .ToList();
+        data.Add(numbers);
+    }
+}
+
+
+
+var part = Environment.GetEnvironmentVariable("part");
+var solution = part switch
+{
+    "part1" => Puzzle.Part1(rules, data),
+    "part2" => Puzzle.Part2(rules, data),
+    _ => throw new ArgumentOutOfRangeException($"unexpected part {part}")
+};
+
+Console.WriteLine(solution);
---
+++ day05/Puzzle.cs
@@ -0,0 +1,39 @@
+﻿namespace day05;
+
+public class Puzzle
+{
+    public static int Part1(List<(int, int)> rules, List<List<int>> data)
+    {
+        return data.Where(row => IsValidRow(rules, row)).Select(row => row.ElementAt(row.Count / 2)).Sum();
+    }
+
+    private static bool IsValidRow(List<(int, int)> rules, List<int> row)
+    {
+        var rowPos = row.Select((x, i) => (x, i)).ToDictionary(x => x.x, x => x.i);
+        
+        return rules.Where(rule => rowPos.ContainsKey(rule.Item1) && rowPos.ContainsKey(rule.Item2)).All(rule => rowPos[rule.Item1] < rowPos[rule.Item2]);
+    }
+    public static int Part2(List<(int, int)> rules, List<List<int>> data)
+    {
+        var invalidRows = data.Where(row => !IsValidRow(rules, row)).ToList();
+        
+        return invalidRows
+            .Select(row => ReorderRow(row, rules))
+            .Select(row => row.ElementAt(row.Count / 2))
+            .Sum();
+    }
+
+    private static List<int> ReorderRow(List<int> row, List<(int, int)> rules)
+    {
+        var elementOrder = row.ToDictionary(element => element, after => new List<int>());
+        foreach (var rule in rules)
+        {
+            if (row.Contains(rule.Item1) && row.Contains(rule.Item2))
+            {
+                elementOrder[rule.Item1].Add(rule.Item2);
+            }
+        }
+        
+        return row.OrderBy(r => elementOrder[r].Count).ToList();
+    }
+}
