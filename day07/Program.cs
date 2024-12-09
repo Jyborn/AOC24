8a0f84fc023fd63b30675acfc40406588e27e164
Jyborn <alexander.jyborn@gmail.com>
2024-12-09 12:42:19 +0100

    day7

---
+++ day07/Program.cs
@@ -0,0 +1,23 @@
+﻿using day07;
+
+using FileStream fs = new("input.txt", FileMode.Open, FileAccess.Read);
+using StreamReader sr = new StreamReader(fs);
+var numbers = new Dictionary<long, long[]>();
+string line;
+while ((line = sr.ReadLine()) != null)
+{
+    var parts = line.Split(':');
+    var total = long.Parse(parts[0].Trim());
+    var terms = Array.ConvertAll(parts[1].Trim().Split(' '), long.Parse);
+    numbers.Add(total, terms);
+}
+
+var part = Environment.GetEnvironmentVariable("part");
+var solution = part switch
+{
+    "part1" => Puzzle.Part1(numbers),
+    "part2" => Puzzle.Part2(numbers),
+    _ => throw new ArgumentOutOfRangeException($"unexpected part {part}")
+};
+
+Console.WriteLine(solution);
---
+++ day07/Puzzle.cs
@@ -0,0 +1,61 @@
+﻿namespace day07;
+
+public static class Puzzle
+{
+    public static long Part1(Dictionary<long, long[]> numbers)
+    {
+        return numbers.ToList().Where(x => IsValidEquation(x, ['*', '+'])).Sum(x => x.Key);
+    }
+
+    private static bool IsValidEquation(KeyValuePair<long, long[]> numbers, char[] symbols)
+    {
+        var target = numbers.Key;
+        var terms = numbers.Value;
+        
+        var operatorOrders = GetCombinations(symbols, terms.Length - 1);
+
+        foreach (var operators in operatorOrders)
+        {
+            var total = terms[0];
+            for (var i = 0; i < operators.Length; i++)
+            {   
+                switch (operators[i])
+                {
+                    case '+': total += terms[i + 1]; break;
+                    case '*': total *= terms[i + 1]; break;
+                    case '|': total = long.Parse(total.ToString() + terms[i + 1]); break;
+                }   
+            }
+            if (total == target) return true;
+        }
+        return false;
+    }
+
+    private static List<char[]> GetCombinations(char[] symbols, int length)
+    {
+        var results = new List<char[]>();
+        var combination = new char[length];
+        GenerateCombinations(symbols, combination, 0, results);
+        return results;
+    }
+    
+    private static void GenerateCombinations(char[] symbols, char[] combination, int position, List<char[]> results)
+    {
+        if (position == combination.Length)
+        {
+            results.Add((char[])combination.Clone());
+            return;
+        }
+
+        foreach (var symbol in symbols)
+        {
+            combination[position] = symbol;
+            GenerateCombinations(symbols, combination, position + 1, results);
+        }
+    }
+    public static long Part2(Dictionary<long, long[]> numbers)
+    {
+        
+        return numbers.ToList().Where(x => IsValidEquation(x, ['*', '+', '|'])).Sum(x => x.Key);
+    }
+}
