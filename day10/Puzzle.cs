83b773f50a6a777afbeaa69d458281086891b46b
Jyborn <alexander.jyborn@gmail.com>
2024-12-10 14:47:24 +0100

    basic day project template

---
+++ day10/Puzzle.cs
@@ -0,0 +1,14 @@
+﻿namespace day10;
+
+public class Puzzle
+{
+    public static int Part1(string input)
+    {
+        return 0;
+    }
+
+    public static int Part2(string input)
+    {
+        return 0;
+    }
+}
