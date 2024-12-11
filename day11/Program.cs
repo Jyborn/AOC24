ca272ba2de2351d2fa3cdaaa972851398ecb0cd1
Jyborn <alexander.jyborn@gmail.com>
2024-12-11 18:08:12 +0100

    day11

---
+++ day11/Program.cs
@@ -0,0 +1,17 @@
+﻿using day11;
+
+using FileStream fs = new("input.txt", FileMode.Open, FileAccess.Read);
+using StreamReader sr = new(fs);
+var line = sr.ReadLine() ?? "";
+
+var part = Environment.GetEnvironmentVariable("part");
+var solution = part switch
+{
+    "part1" => Puzzle.Part1(line, 25),
+    "part2" => Puzzle.Part2(line, 75),
+    _ => throw new ArgumentOutOfRangeException($"unexpected part {part}")
+};
+
+Console.WriteLine("\n---------Solution---------");
+Console.WriteLine(solution);
+Console.WriteLine("--------------------------");
---
+++ day11/Puzzle.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace day11;
+
+public class Puzzle
+{
+    public static int Part1(string input, int nBlinks)
+    {
+        var numbers = input.Split(" ").Select(long.Parse).ToList();
+        for (var i = 0; i < nBlinks; i++)
+        {
+            //Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine($"blink {i}");
+            numbers = numbers.SelectMany(number => Blink(number)).ToList();
+        }
+        return numbers.Count;
+    }
+
+
+    private static List<long> Blink(long number)
+    {
+        var numString = number.ToString();
+        if (number == 0) { return [1]; }
+        if (numString.Length % 2 == 0)
+        {
+            var split1 = numString[..(numString.Length / 2)];
+            var split2 = numString[(numString.Length / 2)..].TrimStart('0');
+            return [long.Parse(split1), long.Parse(split2.Length > 0 ? split2 : "0")];
+        }
+        return [number * 2024];
+    }
+    
+    public static long Part2(string input, int nBlinks)
+    {
+        var numbers = input.Split(" ").ToList();
+        
+        //<number, occurrences>
+        var numberOccs = numbers.Select(long.Parse).GroupBy(x => x).ToDictionary(g => g.Key, g => (long) g.Count());
+
+        for (var i = 0; i < nBlinks; i++)
+        {
+            numberOccs.ToList().ForEach(x =>
+            {
+                var newNumbs = Blink(x.Key);
+                var occs = x.Value;
+                numberOccs[x.Key] -= occs;
+                foreach (var newNumb in newNumbs)
+                {
+                    if (!numberOccs.ContainsKey(newNumb))
+                        numberOccs[newNumb] = occs;
+                    else
+                        numberOccs[newNumb] += occs;
+                }
+                
+                if (numberOccs[x.Key] <= 0)
+                    numberOccs.Remove(x.Key);
+            });
+        }
+        return numberOccs.ToList().Where(x => x.Value > 0).Sum(x => x.Value);
+    }
+}
